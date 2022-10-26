6e96193316f03afddf3395e545fcf6e7348fbbc8
izaias <izaiasenelly@gmail.com>
2022-10-26 12:11:08 -0300

    Desenvolvido o código do programa

---
+++ code.py
@@ -0,0 +1,15 @@
+#CodePark - Atividade 2
+quantRodas = int(input('Digite a quantidade de rodas do veículo: '))
+peso = float(input('Digite o peso do veículo em quilogramas: '))
+quantPessoas = int(input('Digite a quantidade de pessoas suportada do veículo: '))
+
+if(quantRodas <= 3):
+    print('Categoria A')
+elif(quantRodas >= 4 and quantPessoas <= 8 and peso <= 3500):
+    print('Categoria B')
+elif(quantRodas >= 4 and peso >= 3500 and peso <= 6000):
+    print('Categoria C')
+elif(quantRodas >= 4 and quantPessoas > 8):
+    print('Categoria D')
+else:
+    print('Categoria F')
