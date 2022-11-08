45ac3b17a3a443a30d90293f9bed3bcf9ce83047
izaias <izaiasenelly@gmail.com>
2022-11-08 12:25:25 -0300

    Melhorias no laço e tratamento de erros

--- Entrada e saída de dados/CodePark-1.py
+++ Entrada e saída de dados/CodePark-1.py
@@ -1,26 +1,28 @@
-contador = 0
-while (contador == 0):
-    print('1: Soma\n2: Subtração\n3: Multiplicação\n4: Divisão\n0: Sair')
-    op = float(input('Digite uma opção para calcular: '))
-    if (op > 4):
-        print('Essa opção não existe')
-    elif (op == 0):
-        exit()
-    else:
-        n1 = float(input('Digite o primeiro Número: '))
-        n2 = float(input('Digite o segundo número: '))
+while True:
+    try:
+        print('1: Soma\n2: Subtração\n3: Multiplicação\n4: Divisão\n0: Sair')
+        op = float(input('Digite uma opção para calcular: '))
+        if (op > 4):
+            print('Essa opção não existe')
+        elif (op == 0):
+            break
+        else:
+            n1 = float(input('Digite o primeiro Número: '))
+            n2 = float(input('Digite o segundo número: '))
 
-        def calcular(n1, n2, op):
-            if op == 1:
-                soma = n1 + n2
-                return soma
-            elif op == 2:
-                sub = n1 -  n2
-                return sub
-            elif op == 3:
-                mult = n1 * n2
-                return mult
-            elif op == 4:
-                div = n1 / n2
-                return div
-        print(calcular(n1, n2, op))
+            def calcular(n1, n2, op):
+                if op == 1:
+                    soma = n1 + n2
+                    return soma
+                elif op == 2:
+                    sub = n1 -  n2
+                    return sub
+                elif op == 3:
+                    mult = n1 * n2
+                    return mult
+                elif op == 4:
+                    div = n1 / n2
+                    return div
+            print(f'O resultado é: {calcular(n1, n2, op)}')
+    except:
+        print('O dado informado não é válido; tente novamente')
